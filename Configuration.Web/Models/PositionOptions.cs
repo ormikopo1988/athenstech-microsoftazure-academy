c9474930a235c344938ca7b0308273cd692d6c54
Orestis Meikopoulos <ormeikop@microsoft.com>
2020-11-16 20:51:34 +0200

    Configuration in aspnet core, security practices and Azure Key Vault initial commit.

---
+++ Configuration.Web/Models/PositionOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Configuration.Web.Models
+{
+    public class PositionOptions
+    {
+        public const string Position = "Position";
+
+        public string Title { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Configuration.Web/Pages/ReadValueFromArray.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace Configuration.Web.Pages
+{
+    public class ReadValueFromArrayModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+
+        public ReadValueFromArrayModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string FirstServerName { get; private set; }
+        public string SecondServerName { get; private set; }
+
+        public void OnGet()
+        {
+            // From array with index
+            FirstServerName = _configuration["Servers:0:Name"];
+            SecondServerName = _configuration["Servers:1:Name"];
+        }
+    }
+}
